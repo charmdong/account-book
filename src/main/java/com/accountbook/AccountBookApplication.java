623d70c7e38a15a8ecc47d8f01dbd71d8338b97d
Donggun.Chung <chungdk1117@gmail.com>
2021-10-31 22:05:24 +0900

    :tada: 2021.10.31.Sun

---
+++ src/main/java/com/accountbook/AccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accountbook/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
