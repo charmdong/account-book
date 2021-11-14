0e49b559ab25f9d545d521f344e2395b6e6dd2ec
Donggun.Chung <chungdk1117@gmail.com>
2021-11-14 23:36:26 +0900

    :truck: 2021.11.14.Sun [rename class]

--- src/main/java/com/accountbook/api/CategoryApiController.java
+++ src/main/java/com/accountbook/api/CategoryApiController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/categories")
-public class UserCategoryApiController {
+public class CategoryApiController {
 
     private final CategoryService categoryService;
 
