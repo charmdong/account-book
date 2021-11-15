35bf5a46a42303552f81c986488a2bc0fe0eeea0
Donggun.Chung <chungdk1117@gmail.com>
2021-11-15 21:42:48 +0900

    :hammer: 2021.11.15.Mon [주석 추가]

--- src/main/java/com/accountbook/domain/repository/category/CategoryRepository.java
+++ src/main/java/com/accountbook/domain/repository/category/CategoryRepository.java
@@ -19,6 +19,12 @@ public class CategoryRepository {
 
     private final EntityManager em;
 
+    /**
+     * 사용자 카테고리 목록 조회
+     *
+     * @param userId
+     * @return 사용자 카테고리 목록
+     */
     public List<Category> getUserCategoryList (String userId) {
         return em.createQuery(
                 "select c from Category c where c.user =: userId", Category.class)
@@ -26,16 +32,32 @@ public class CategoryRepository {
                 .getResultList();
     }
 
+    /**
+     * 사용자 카테고리 상세 조회
+     *
+     * @param seq
+     * @return 사용자 카테고리 상세 정보
+     */
     public Category getCategory (Long seq) {
 
         return em.find(Category.class, seq);
     }
 
+    /**
+     * 사용자 카테고리 등록
+     *
+     * @param category
+     */
     public void addCategory (Category category) {
 
         em.persist(category);
     }
 
+    /**
+     * 사용자 카테고리 삭제
+     *
+     * @param category
+     */
     public void deleteCategory (Category category) {
 
         em.remove(category);
--- src/main/java/com/accountbook/domain/repository/category/ComCategoryRepository.java
+++ src/main/java/com/accountbook/domain/repository/category/ComCategoryRepository.java
@@ -6,5 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComCategoryRepository extends JpaRepository<ComCategory, Long> {
 
+    /**
+     * 카테고리 공통 코드 정보 조회
+     *
+     * @param name
+     * @param eventType
+     * @return
+     */
     ComCategory findByNameAndEventType(String name, EventType eventType);
 }
