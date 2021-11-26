7a743cd63bc23299b461afcd45e2d57b41bcff0d
Donggun.Chung <chungdk1117@gmail.com>
2021-11-26 19:31:27 +0900

    :clown_face: 2021.11.26.Fri [UserService test]

--- src/test/java/com/accountbook/service/UserServiceTest.java
+++ src/test/java/com/accountbook/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.accountbook.service;
 
+import com.accountbook.dto.user.UserDto;
+import com.accountbook.dto.user.UserRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Year;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-class UserServiceTest {
+public class UserServiceTest {
 
     @Autowired
     UserService userService;
@@ -20,8 +24,19 @@ class UserServiceTest {
     @Test
     public void addUserTest () throws Exception {
         // given
+        UserRequest request = new UserRequest();
+
+        request.setId("test1");
+        request.setPassword("password1");
+        request.setName("Donggun");
+        request.setEmail("chungdk1993@naver.com");
+        request.setBirthDate(Year.of(1993).atMonth(11).atDay(17).atTime(14,59));
+
+        userService.addUser(request);
 
         // when
+        UserDto user = userService.getUser(request.getId());
+        System.out.println("user = " + user);
 
         // then
     }
