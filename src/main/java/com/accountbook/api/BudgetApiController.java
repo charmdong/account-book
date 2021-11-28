6f6c6768d25605a40f885f7373f136f5fb457b0b
Soomin Kim <soomin0403@naver.com>
2021-11-29 00:00:23 +0900

    2021.11.28 [Budget 서비스ApiResponse 추가]

--- src/main/java/com/accountbook/api/BudgetApiController.java
+++ src/main/java/com/accountbook/api/BudgetApiController.java
@@ -2,8 +2,10 @@ package com.accountbook.api;
 
 import com.accountbook.dto.Budget.BudgetDto;
 import com.accountbook.dto.Budget.BudgetRequest;
+import com.accountbook.dto.asset.ApiResponse;
 import com.accountbook.service.BudgetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,27 +19,37 @@ public class BudgetApiController {
 
     //예산 전체 조회
     @GetMapping("/")
-    public List<BudgetDto> getAllBudget(){
-        return budgetService.getAllBudget();
+    public ApiResponse getAllBudget(){
+        List<BudgetDto> budgetDtoList = budgetService.getAllBudget();
+        ApiResponse apiResponse = new ApiResponse(budgetDtoList, HttpStatus.OK, "SUCCESS");
+        return apiResponse;
     }
     //예산 상세 조회
     @GetMapping("/{budgetSeq}")
-    public BudgetDto getOneBudget(@PathVariable long budgetSeq){
-        return budgetService.getOneBudget(budgetSeq);
+    public ApiResponse getOneBudget(@PathVariable long budgetSeq){
+        BudgetDto buegetdto = budgetService.getOneBudget(budgetSeq);
+        ApiResponse apiResponse = new ApiResponse(buegetdto, HttpStatus.OK, "SUCCESS");
+        return apiResponse;
     }
     //예산 등록
     @PostMapping("/")
-    public void create(@RequestBody @Valid BudgetRequest budgetRequest){
-        budgetService.enrollBudget(budgetRequest);
+    public ApiResponse create(@RequestBody @Valid BudgetRequest budgetRequest){
+        Long buegetSeq = budgetService.enrollBudget(budgetRequest);
+        ApiResponse apiResponse = new ApiResponse(buegetSeq, HttpStatus.OK, "SUCCESS");
+        return apiResponse;
     }
     //예산 수정
     @PatchMapping("/{budgetSeq}")
-    public void update(@RequestBody @Valid BudgetRequest budgetRequest,@PathVariable long budgetSeq){
-        budgetService.updateBudget(budgetRequest, budgetSeq);
+    public ApiResponse update(@RequestBody @Valid BudgetRequest budgetRequest,@PathVariable long budgetSeq){
+        BudgetDto budgetDto = budgetService.updateBudget(budgetRequest, budgetSeq);
+        ApiResponse apiResponse = new ApiResponse(budgetDto, HttpStatus.OK, "SUCCESS");
+        return apiResponse;
     }
     //예산 삭제
     @DeleteMapping("/{budgetSeq}")
-    public void delete(@PathVariable long budgetSeq){
-        budgetService.deleteBudget(budgetSeq);
+    public ApiResponse delete(@PathVariable long budgetSeq){
+        Boolean result = budgetService.deleteBudget(budgetSeq);
+        ApiResponse apiResponse = new ApiResponse(result, HttpStatus.OK, "SUCCESS");
+        return apiResponse;
     }
 }
