b12a58fbf315b2da60f520e411c7562cf02e1eea
Donggun.Chung <chungdk1117@gmail.com>
2021-12-06 22:17:00 +0900

    :hammer: 2021.12.06.Mon [add CategoryExceptionCode]

---
+++ src/main/java/com/accountbook/exception/category/CategoryExceptionCode.java
@@ -0,0 +1,12 @@
+package com.accountbook.exception.category;
+
+/**
+ * CategoryExceptionCode
+ *
+ * @author donggun
+ * @since 2021/12/06
+ */
+public class CategoryExceptionCode {
+
+    private static final int FAIL = 100;
+}
