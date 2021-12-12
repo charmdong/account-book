bd59787d7f61b033ac7dc72a9822a3b8dc7594df
KimMingeon0625 <machine.kmg@gmail.com>
2021-12-13 01:36:08 +0900

    fix assetservice

--- src/main/java/com/accountbook/service/AssetService.java
+++ src/main/java/com/accountbook/service/AssetService.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,34 +21,44 @@ public class AssetService {
     private final AssetRepository assetRepository;
 
     // 자산 전체 조회
-    public List<AssetDto> getAssetList(String userId) {
+    public List<AssetDto> getAssetList(String userId) throws Exception {
         List<AssetDto> ret = AssetDto.convertAssetList(assetRepository.findByUserId(userId));
-
         return ret;
     }
+
     // 자산 상세 조회
-    public AssetDto getAsset(long assetSeq) {
+    public AssetDto getAsset(long assetSeq) throws Exception {
         AssetDto ret = new AssetDto(assetRepository.findById(assetSeq).orElseThrow());
         return ret;
     }
 
     // 자산 등록
-    @Transactional
-    public void registAsset(AssetRequest assetRequest) {
+    @Transactional(readOnly = false)
+    public Boolean registAsset(AssetRequest assetRequest) throws Exception {
+        Boolean result = Boolean.FALSE;
         Asset asset = Asset.createAsset(assetRequest);
         assetRepository.save(asset);
+        assetRepository.flush();
+        
+        return result;
     }
 
     // 자산 수정
-    @Transactional
-    public void updateAseet(long assetSeq, AssetRequest assetRequest) {
+    @Transactional(readOnly = false)
+    public AssetDto updateAseet(long assetSeq, AssetRequest assetRequest) throws Exception {
         Asset asset = assetRepository.findById(assetSeq).orElseThrow();
         asset.changeAsset(assetRequest);
+        assetRepository.flush();
+
+        // 업데이트 확인용 조회
+        AssetDto assetDto = new AssetDto(assetRepository.findById(assetSeq).orElseThrow());
+        return assetDto;
     }
 
     // 자산 삭제
-    @Transactional
-    public void removeAsset(long assetSeq) {
+    @Transactional(readOnly = false)
+    public Boolean removeAsset(long assetSeq) throws Exception {
         assetRepository.deleteById(assetSeq);
+        return Boolean.TRUE;
     }
 }
