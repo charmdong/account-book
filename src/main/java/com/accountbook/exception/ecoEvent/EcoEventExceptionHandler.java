20db1c29705d159cca4a17b74d923034992f7434
Soomin Kim <klynn0403@gmail.com>
2022-01-16 22:06:26 +0900

    [2022.01.16 exception handler @Order 추가 -ecoEvent]

--- src/main/java/com/accountbook/exception/ecoEvent/EcoEventExceptionHandler.java
+++ src/main/java/com/accountbook/exception/ecoEvent/EcoEventExceptionHandler.java
@@ -3,14 +3,14 @@ package com.accountbook.exception.ecoEvent;
 import com.accountbook.api.EcoEventApiController;
 import com.accountbook.dto.response.ApiResponse;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
 @RestControllerAdvice(basePackageClasses = {EcoEventApiController.class})
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class EcoEventExceptionHandler {
 
     @ExceptionHandler
