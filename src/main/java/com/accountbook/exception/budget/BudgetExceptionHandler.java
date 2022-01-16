e4f91fd7f5edc8b59a845d4278677447817aaeec
Soomin Kim <klynn0403@gmail.com>
2022-01-16 22:05:08 +0900

    [2022.01.16 exception handler @Order 추가]

--- src/main/java/com/accountbook/exception/budget/BudgetExceptionHandler.java
+++ src/main/java/com/accountbook/exception/budget/BudgetExceptionHandler.java
@@ -3,13 +3,14 @@ package com.accountbook.exception.budget;
 import com.accountbook.api.BudgetApiController;
 import com.accountbook.dto.response.ApiResponse;
 
-import com.accountbook.exception.common.CommonExceptionCode;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = {BudgetApiController.class})
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class BudgetExceptionHandler {
 
     @ExceptionHandler
