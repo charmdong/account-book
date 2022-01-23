937c28fc6d244c7efef79069e7b7816a9bd535a7
Soomin Kim <klynn0403@gmail.com>
2022-01-23 14:52:46 +0900

    2022.01.23 [@Order 추가]

--- src/main/java/com/accountbook/exception/ecoEvent/EcoEventExceptionHandler.java
+++ src/main/java/com/accountbook/exception/ecoEvent/EcoEventExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = {EcoEventApiController.class})
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(Ordered.HIGHEST_PRECEDENCE) 
 public class EcoEventExceptionHandler {
 
     @ExceptionHandler
