3946a6586b60606951b027cf3c12004b2c4352c0
Soomin Kim <klynn0403@gmail.com>
2022-04-12 23:58:58 +0900

    2022.04.12 [Budget 도메인 삭제]

--- src/main/java/com/accountbook/domain/repository/budget/BudgetRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.accountbook.domain.repository.budget;
-
-import com.accountbook.domain.entity.Budget;
-
-public interface BudgetRepositoryCustom {
-    //예산 저장
-    void saveBudget(Budget budget);
-}
