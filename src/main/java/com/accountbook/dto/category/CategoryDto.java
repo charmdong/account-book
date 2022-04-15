5a8192804fb90355075df958db6bb2c260952779
Mingeon <55868455+machine-geon@users.noreply.github.com>
2022-04-16 01:59:14 +0900

    entity 변경

--- src/main/java/com/accountbook/dto/category/CategoryDto.java
+++ src/main/java/com/accountbook/dto/category/CategoryDto.java
@@ -1,25 +1,31 @@
 package com.accountbook.dto.category;
 
 import com.accountbook.domain.entity.Category;
+import com.accountbook.domain.entity.EcoEvent;
 import com.accountbook.domain.enums.EventType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CategoryDto {
 
     private Long seq;
     private String name;
     private EventType eventType;
-    private Boolean useYn;
+    private long defaultPrice;
+    private List<EcoEvent> ecoEventList = new ArrayList<>();
 
     public CategoryDto(Category category) {
         this.seq = category.getSeq();
-        this.name = category.getComCategory().getName();
-        this.eventType = category.getComCategory().getEventType();
-        this.useYn = category.getComCategory().getUseYn();
+        this.name = category.getName();
+        this.eventType = category.getEventType();
+        this.defaultPrice = category.getDefaultPrice();
+        this.ecoEventList = category.getEcoEventList();
     }
 }
