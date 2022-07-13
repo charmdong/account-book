3d99ec7441a76c8e8fc3c478c51d5296f12d68e6
Donggun.Chung <chungdk1117@gmail.com>
2022-07-14 01:01:31 +0900

    :hammer: 2022.07.14.Thu [엔티티 수정자 아이디 호출 로직 수정]

--- src/main/java/com/accountbook/AccountBookApplication.java
+++ src/main/java/com/accountbook/AccountBookApplication.java
@@ -9,8 +9,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @EnableScheduling
@@ -19,25 +23,36 @@ import java.util.Optional;
 @Import(LogTraceAspect.class)
 public class AccountBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountBookApplication.class, args);
-	}
+    private final String NO_LOGIN_USER = "NO_LOGIN_USER";
 
-	/**
-	 * LastModifiedBy 어노테이션이 붙은 필드를 가지고 있는 엔티티가 수정되는 경우 호출됨
-	 * @return 사용자 ID
-	 */
-	@Bean
-	public AuditorAware<String> auditorProvider() {
-		// HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		// HttpSession session = request.getSession();
-		// String loginId = (String) session.getAttribute("loginId");
+    public static void main (String[] args) {
+        SpringApplication.run(AccountBookApplication.class, args);
+    }
 
-		return () -> Optional.of("test");
-	}
+    /**
+     * LastModifiedBy 어노테이션이 붙은 필드를 가지고 있는 엔티티가 수정되는 경우 호출됨
+     *
+     * @return 사용자 ID
+     */
+    @Bean
+    public AuditorAware<String> auditorProvider () {
 
-	@Bean
-	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
-	}
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            HttpSession session = request.getSession();
+
+            if (session != null) {
+                return () -> Optional.of((String) session.getAttribute("loginId"));
+            }
+            else return () -> Optional.of(NO_LOGIN_USER);
+        }
+        else return () -> Optional.of(NO_LOGIN_USER);
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory (EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
