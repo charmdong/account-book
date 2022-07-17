1e0763383a3696d6b31e29b1a35ec4541d7b8434
Donggun.Chung <chungdk1117@gmail.com>
2022-07-17 21:39:17 +0900

    :hammer: 2022.07.17.Sun [Exception Code 추가]

--- src/main/java/com/accountbook/exception/user/UserExceptionCode.java
+++ src/main/java/com/accountbook/exception/user/UserExceptionCode.java
@@ -13,13 +13,15 @@ public enum UserExceptionCode {
 
     INSERT_FAIL (3000L, "사용자 등록에 실패했습니다."),
     SETTING_FAIL(3001L, "사용자 설정에 실패했습니다."),
-    NOT_FOUND (3002L, "사용자 조회에 실패했습니다."),
+    NOT_FOUND (3002L, "사용자 정보가 없습니다."),
     UPDATE_FAIL (3003L, "사용자 수정에 실패했습니다."),
     DELETE_FAIL (3004L, "사용자 삭제에 실패했습니다."),
     PWD_UPDATE_FAIL (3005L, "패스워드 변경에 실패했습니다."),
     INVALID_PWD (3006L, "패스워드가 일치하지 않습니다."),
     INVALID_SESSION_ID (3007L, "세션 아이디가 일치하지 않습니다."),
     SETTING_NOT_FOUND(3008L, "사용자 설정 정보 조회에 실패했습니다."),
+    PRESENT_USER_ID(3009L, "이미 사용 중인 아이디입니다."),
+    PRESENT_USER_EMAIL(3010L, "이미 사용 중인 이메일입니다."),
 
     ;
 
